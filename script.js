b43befcc84014ceeaf2e2f0a12fd1d1cccb4bc20
Laurenz <ljdsuarez@gmail.com>
2023-12-03 12:02:15 +0300

    updated

--- script.js
+++ script.js
@@ -6,27 +6,8 @@ window.onscroll = function () {
 
 function scrollFunction() {
   if (document.body.scrollTop > 35 || document.documentElement.scrollTop > 35) {
-    document.getElementById("navline").style.width = "100vw";
+    document.getElementById("navline").style.opactiy = "0";
   } else {
-    document.getElementById("navline").style.width = "0vw";
+    document.getElementById("navline").style.opacity = "100";
   }
-}
-
-//for works script
-
-document.getElementById("lps").style.fontWeight = "700";
-document.getElementById("showcase-tcs").style.display = "none";
-
-//selecting class function styling
-function classStyle(className, styleName, value) {
-  for (let i = 0; i < document.getElementsByClassName(className).length; i++) {
-    document.getElementsByClassName(className)[i].style[styleName] = value;
-  }
-}
-
-function displayShowcase(clickedId) {
-  classStyle("showcase", "display", "none");
-  document.getElementById(`showcase-${clickedId}`).style.display = "flex";
-  classStyle("work-title", "fontWeight", "300");
-  document.getElementById(`${clickedId}`).style.fontWeight = "700";
 }
